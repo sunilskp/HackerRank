da8c0eafa75355c3209759be5d670b12e642b7d1
sunilskp <44226373+sunilskp@users.noreply.github.com>
2020-06-02 22:12:45 +0530

    Update BreakingtheRecords.cpp

--- BreakingtheRecords.cpp
+++ BreakingtheRecords.cpp
@@ -4,10 +4,10 @@ Breaking the Records
 
 
 vector<int> breakingRecords(vector<int> scores) {
-    int max = scores[0];
-    int min = scores[0];
-    int count_max = 0;
-    int count_min = 0;
+    int max = scores[0]; // to store the max record 
+    int min = scores[0]; // to store the min record 
+    int count_max = 0; // to count the number of time she broke her best record 
+    int count_min = 0; // to count the number of time she broke her worst record
     vector <int> re ;
     int len  = scores.size();
     for (int i=0; i<len; i++)
