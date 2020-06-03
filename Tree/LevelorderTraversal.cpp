61f9b9a5780f806dbb02977452816a8af68bdf27
sunilskp <44226373+sunilskp@users.noreply.github.com>
2020-06-03 20:12:04 +0530

    Create LevelorderTraversal.cpp

---
+++ Tree/LevelorderTraversal.cpp
@@ -0,0 +1,25 @@
+ /*
+ Level Order Traversal in C++
+ */
+ 
+ void levelOrder(Node * root) {
+        queue<Node *> S;
+        S.push(root);
+        Node *temp = NULL;
+        while(!S.empty())
+        {   
+            temp = S.front();
+            S.pop();
+            cout<<temp->data<<" ";
+            if(temp->left)
+            {
+                S.push(temp->left);
+            }
+            if(temp->right)
+            {
+                S.push(temp->right);
+            }
+        }
+      
+    }
+
