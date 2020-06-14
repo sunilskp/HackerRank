5f1f3ab1369a76ab339dc8883c6bd7b8e1606258
Sunil Kumar Palariya <44226373+sunilskp@users.noreply.github.com>
2020-06-14 23:43:04 +0530

    Update CycleDetection.cpp

--- LinkedList/CycleDetection.cpp
+++ LinkedList/CycleDetection.cpp
@@ -1,1 +1,24 @@
+  /*
+We will use the Hare and Tortoise Algo
+  */
 
+bool has_cycle(SinglyLinkedListNode* head) {
+SinglyLinkedListNode *t = head;
+SinglyLinkedListNode *r = head;
+if(head == NULL || head->next==NULL)
+{
+    return false;
+}
+while( r!=NULL&&r->next!=NULL)
+{
+    t = t->next;   // Tortoise Node
+    r = r->next->next; // Hare Node
+    if(t==r)
+    {
+        return true;
+        break;
+    }
+
+}
+return false;
+}
